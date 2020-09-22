2891a18cee8dbaf03c54155d3d098f2c6f24ab14
hmiyachi <hmiyachi@gijuhrdsad.local>
2020-09-22 10:57:25 +0900

    props

--- src/App.js
+++ src/App.js
@@ -10,10 +10,26 @@ import React from 'react';
 // }
 
 const App = () => {
-  return (<div><Cat /><Cat /></div>)
+  const profiles=[
+    {name:"Taro", age:10},
+    {name:"Hanako", age:12},
+    {name:"NoName"}    
+  ]
+  return (
+    <div>
+      {
+      profiles.map((profile, index)=> {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
+    </div>
+  )
 }
-const Cat = () => {
-  return <div>Mee!</div>
+const User = (props) => {
+return <div>Hi! I am {props.name}!, and {props.age} years old</div>
 }
 
+User.defaultProps ={
+  age:1
+}
 export default App;
