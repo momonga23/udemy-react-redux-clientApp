12bd5441f6d52b16f417f09b0b6fe350e8770435
hmiyachi <hmiyachi@gijuhrdsad.local>
2020-09-22 10:23:11 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello World</h1></div>
-  );
+// function App() {
+//   return (
+//     <React.Fragment>
+//     <label htmlFor="bar">bar</label>
+//     <input type="text" onChange={()=>{console.log("I am clicked")}} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  return (<div><Cat /><Cat /></div>)
+}
+const Cat = () => {
+  return <div>Mee!</div>
 }
 
 export default App;
